09265d5fd8c3a395ea9b767f154539b53e2787ee
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-20 18:51:27 -0400

    fixed race condition issue for settings tests

--- test/settings.js
+++ test/settings.js
@@ -6,8 +6,8 @@ var test     =  require('tap').test
   , fs       =  require('fs')
   , settings =  require('../settings')
   , hideSemicolonsTheme =  require('../themes/hide-semicolons')
-  , home = path.join(__dirname, 'fixtures', 'home')
-  , rcpath = path.join(home, '.cardinalrc')
+  , home                =  path.join(__dirname, 'fixtures', 'home')
+  , rcpath              =  path.join(home, '.cardinalrc')
 
 function writerc(config) {
   fs.writeFileSync(rcpath, JSON.stringify(config), 'utf-8')
@@ -24,20 +24,23 @@ function resolveTheme (config) {
   return result;
 }
 
+// Get all results synchronously to avoid tests from interfering with each other
+
+var norcTheme  =  settings.resolveTheme(home)
+  , namedTheme =  resolveTheme({ theme: "hide-semicolons" })
+  , pathTheme  =  resolveTheme({ theme: path.join(__dirname, '..', 'themes', 'hide-semicolons.js') })
+
 test('no .cardinalrc in home', function (t) {
-  var theme = settings.resolveTheme(home)
-  t.equals(theme, undefined, 'resolves no theme') 
+  t.equals(norcTheme, undefined, 'resolves no theme') 
   t.end()
 })
 
 test('.cardinalrc with theme "hide-semicolons" in home', function (t) {
-  var theme = resolveTheme({ theme: "hide-semicolons" })
-  t.deepEquals(theme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
+  t.deepEquals(namedTheme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
   t.end()
 })
 
 test('.cardinalrc with full path to "hide-semicolons.js" in home', function (t) {
-  var theme = resolveTheme({ theme: path.join(__dirname, '..', 'themes', 'hide-semicolons.js') })
-  t.deepEquals(theme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
+  t.deepEquals(pathTheme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
   t.end()
 })
