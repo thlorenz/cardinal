3546222534f8f647f1ff0a893f2f7a920c28a55e
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-20 18:54:50 -0400

    creating home directory for settings tests

--- test/settings.js
+++ test/settings.js
@@ -24,23 +24,22 @@ function resolveTheme (config) {
   return result;
 }
 
-// Get all results synchronously to avoid tests from interfering with each other
-
-var norcTheme  =  settings.resolveTheme(home)
-  , namedTheme =  resolveTheme({ theme: "hide-semicolons" })
-  , pathTheme  =  resolveTheme({ theme: path.join(__dirname, '..', 'themes', 'hide-semicolons.js') })
+if (!fs.exists(home)) fs.mkdir(home);
 
 test('no .cardinalrc in home', function (t) {
-  t.equals(norcTheme, undefined, 'resolves no theme') 
+  var theme = settings.resolveTheme(home)
+  t.equals(theme, undefined, 'resolves no theme') 
   t.end()
 })
 
 test('.cardinalrc with theme "hide-semicolons" in home', function (t) {
-  t.deepEquals(namedTheme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
+  var theme = resolveTheme({ theme: "hide-semicolons" })
+  t.deepEquals(theme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
   t.end()
 })
 
 test('.cardinalrc with full path to "hide-semicolons.js" in home', function (t) {
-  t.deepEquals(pathTheme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
+  var theme = resolveTheme({ theme: path.join(__dirname, '..', 'themes', 'hide-semicolons.js') })
+  t.deepEquals(theme, hideSemicolonsTheme, 'resolves hide-semicolons theme') 
   t.end()
 })
