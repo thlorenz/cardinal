6ef47b79db2c3c5ce23914c404a4562ee292bd7d
Thorsten Lorenz <thlorenz@gmx.de>
2012-11-03 18:02:31 -0400

    setting linenumber printing
    - can be set in .cardinalrx
    - if turned on that can be turned off via --nonum

--- bin/cdl.js
+++ bin/cdl.js
@@ -4,12 +4,28 @@ var cardinal = require('..')
   , settings = require('../settings')
   , args = process.argv
   , theme = settings.resolveTheme()
+  , opts = settings.getSettings()
   , highlighted
   ;
 
+opts.theme = theme;
+
+function usage() {
+  var msg = [ 
+      'Usage: cdl <filename.js> [options]'
+    , ''
+    , 'Options (~/.cardinalrc overrides):'
+    , '  --nonum: turn off line printing'
+    , ''
+    , 'Unix Pipe Example: cat filename.js | grep console | cdl'
+    , ''
+  ].join('\n');
+  console.log(msg);
+}
+
 function highlightFile () {
   try {
-    highlighted = cardinal.highlightFileSync(args[2], { theme: theme });
+    highlighted = cardinal.highlightFileSync(args[2], opts);
     console.log(highlighted);
   } catch (e) {
     console.trace();
@@ -20,9 +36,26 @@ function highlightFile () {
 // E.g., "cardinal myfile.js"
 if (args.length === 3) return highlightFile();
 
+var opt = args[3];
+
+// E.g., "cardinal myfile.js --nonum"
+if (opt && opt.indexOf('--') === 0 ) {
+  if ((/^--(nonum|noline)/i).test(opt)) opts.linenos = false;
+  else { 
+    usage();
+    return console.error('Unknown option: ', opt);
+  }
+ 
+  return highlightFile();
+}
+
+
 // UNIX pipes e.g., "cat myfile.js | grep console | cardinal
 var stdin = process.stdin
   , stdout = process.stdout;
+
+// line numbers don't make sense when we are printing line by line
+opts.linenos = false;
 
 stdin.resume();
 stdin.setEncoding('utf-8');
@@ -30,7 +63,7 @@ stdin
   .on('data', function (chunk) {
     chunk.split('\n').forEach(function (line) {
       try {
-        stdout.write(cardinal.highlight(line, theme) + '\n');
+        stdout.write(cardinal.highlight(line, opts) + '\n');
       } catch (e) {
         // line doesn't represent a valid js snippet and therefore cannot be parsed -> just print as is
         stdout.write(line + '\n');
--- settings.js
+++ settings.js
@@ -2,42 +2,50 @@ var path =  require('path')
   , util =  require('util')
   , fs   =  require('fs')
   , utl  =  require('./utl')
-  , home =  process.env.HOME;
+  , home =  process.env.HOME
+  , settings;
 
-// home_ mainly to be used during tests
-// Resolves the preferred theme from the .cardinalrc found in the HOME directory
-// If it couldn't be resolved, undefined is returned
-function resolveTheme (home_) {
-  var settingsJson
-    , settings
-    , themePath;
-
+function getSettings (home_) {
+  if (settings) return settings;
   try {
     settingsJson = fs.readFileSync(path.join(home_ || home, '.cardinalrc'), 'utf-8');
   } catch (_) {
     // no .cardinalrc found - not a problem
     return undefined;
   }
+  try {
+    return JSON.parse(settingsJson);
+  } catch (e) {
+    // Have a .cardinalrc, but something about it is wrong - warn the user
+    // Coudn't parse the contained JSON
+    console.error(e);
+    return undefined;
+  }
+}
+
+// home_ mainly to be used during tests
+// Resolves the preferred theme from the .cardinalrc found in the HOME directory
+// If it couldn't be resolved, undefined is returned
+function resolveTheme (home_) {
+  var themePath
+    , settings = getSettings(home_);
+
+  if (!settings || !settings.theme) return undefined;
 
   try {
-
-    settings = JSON.parse(settingsJson);
-
-    if (!settings.theme) return undefined;
-    
     // allow specifying just the name of a built-in theme or a full path to a custom theme
     themePath = utl.isPath(settings.theme) ? settings.theme : path.join(__dirname, 'themes', settings.theme);
 
     return require(themePath);
   } catch (e) {
-    // Have a .cardinalrc, but something about it is wrong - warn the user
-    // Either we couldn't parse the contained JSON, or the specified theme path is invalid
+    // Specified theme path is invalid
     console.error(e);
     return undefined;
   }
 }
 
 module.exports = {
-  resolveTheme: resolveTheme
+    resolveTheme: resolveTheme
+  , getSettings: getSettings
 };
 
