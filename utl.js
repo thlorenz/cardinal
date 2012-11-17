47eed29ac4d3f8855a232a7c72a8e309c5cdb4f2
Thorsten Lorenz <thlorenz@gmx.de>
2012-11-17 12:02:29 -0500

    added inspect to utl

--- utl.js
+++ utl.js
@@ -1,8 +1,11 @@
-function isPath (s) {
-  return (/[\/\\]/).test(s);
-}
+var util = require('util');
 
-module.exports = {
-  isPath: isPath
+module.exports.isPath = function (s) {
+  return (/[\/\\]/).test(s);
 };
+
+module.exports.inspect = function(obj, depth) {
+  console.log(util.inspect(obj, false, depth || 5, true));
+};
+
 
